6007252f2b3707fa892c38e4b3d2ee71fc35fb9c
Mark Livingston <livingma@yahoo.com>
2015-08-18 11:49:47 -0400

    minor changes

--- autodeploymentAPI.rb
+++ autodeploymentAPI.rb
@@ -127,7 +127,7 @@ if __FILE__ == $0
     puts "*****Welcome to the Automated Deployment Encryption Tools*****"; puts
     puts "This tools will prompt the user for the center and application name."; puts
     puts " Generate Keys - this is a one time only command that will generate public and private keys.\n"
-    puts " This functionality has to commands, the _alt version also you to pass arguments while bypassing prompts.\n"
+    puts " This functionality has two commands, the _alt version allows you to pass arguments while bypassing prompts.\n"
     puts "   Usage: ruby autodeploymentAPI generatekeys"
     puts "   Usage: ruby autodeploymentAPI generatekeys <center> <application>"; puts
     puts " Encrypt Data - you pass a string argument and it encrypt it then encodes the value then dumps it to the screen.\n For example, if it is a password simply copy the output to the LST/CFG for the value of that property."
